6d3ae36953d47a6cfc677f100f70d44cbce1db54
Lucas Willemot <willemotlucas@gmail.com>
2017-01-04 22:37:24 +0100

    Adding styles to navigation bar and tab bar to stick our specifications

--- components/TabIcon.js
+++ components/TabIcon.js
@@ -1,22 +1,34 @@
-import React, {
-  PropTypes,
-} from 'react';
-import {
-  Text,
-} from 'react-native';
+import React, { PropTypes } from 'react';
+import { Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const propTypes = {
   selected: PropTypes.bool,
   title: PropTypes.string,
 };
 
-const TabIcon = (props) => (
-  <Text
-    style={{ color: props.selected ? 'red' : 'black' }}
-  >
-    {props.title}
-  </Text>
-);
+const TabIcon = (props) => {
+	var iconName = "";
+	switch(props.title) {
+		case "Calendrier": iconName = "calendar-o";
+		break;
+		case "Rechercher": iconName = "search";
+		break;
+		case "Profil": iconName = "user";
+		break;
+		case "Messages": iconName = "comment-o";
+		break;
+	}
+
+	return (
+		<View style={{alignItems: 'center'}}>
+			<Icon name={iconName} size={20} color={props.selected ? '#00A799' : 'grey'}/>
+			<Text style={{ color: props.selected ? '#00A799' : 'grey' }} >
+			    {props.title}
+			</Text>
+		</View>
+	)
+};
 
 TabIcon.propTypes = propTypes;
 
--- index.android.js
+++ index.android.js
@@ -18,28 +18,36 @@ import TabView from './components/TabView';
 import TabIcon from './components/TabIcon';
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: 'transparent', justifyContent: 'center',
+  container: { 
+    flex: 1, 
+    backgroundColor: 'transparent', 
+    justifyContent: 'center',
     alignItems: 'center',
   },
   tabBarStyle: {
     backgroundColor: '#F6F6F6',
+    borderTopColor: 'grey',
   },
-  tabBarSelectedItemStyle: {
+  navigationBarStyle: {
     backgroundColor: '#00A799',
+    borderBottomColor: 'transparent',
   },
+  titleStyle: {
+    color: 'white',
+  }
 });
 
 const scenes = Actions.create(
   <Scene key="modal" component={Modal}>
-    <Scene key="root" hideNavBar={true}>
-      <Scene key="login" component={Login} initial={true} title="Connexion"/>
-      <Scene key="register" component={Register} title="Inscription"/>
-      <Scene key="tabbar" tabBarStyle={styles.tabBarStyle} tabBarSelectedItemStyle={styles.tabBarSelectedItemStyle}>
-        <Scene key="main" tabs>
-          <Scene key="calendar" icon={TabIcon} component={Calendar} title="Calendrier"/>
-          <Scene key="search" icon={TabIcon} component={Search} title="Rechercher" initial={true}/>
-          <Scene key="profile" icon={TabIcon} component={Profile} title="Profil"/>
-          <Scene key="messages" icon={TabIcon} component={Messages} title="Messages"/>
+    <Scene key="root">
+      <Scene key="login" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} component={Login} initial title="Connexion"/>
+      <Scene key="register" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} component={Register} title="Inscription"/>
+      <Scene key="tabbar">
+        <Scene key="main" tabs tabBarStyle={styles.tabBarStyle}>
+          <Scene key="calendar" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Calendar} title="Calendrier"/>
+          <Scene key="search" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Search} title="Rechercher" initial={true}/>
+          <Scene key="profile" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Profile} title="Profil"/>
+          <Scene key="messages" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Messages} title="Messages"/>
         </Scene>
       </Scene>
     </Scene>
