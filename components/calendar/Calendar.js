c8dd85dd28f460727fd0058e78a884986cf5374b
Alexandra DUVAL <duvalale@etu.utc.fr>
2017-01-05 20:53:20 +0100

    added calendar module

--- components/calendar/Calendar.js
+++ components/calendar/Calendar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from "react-native";
 import {Actions} from "react-native-router-flux";
+import CalendarComponent from 'react-native-calendar';
 
 var styles = StyleSheet.create({
     container: {
@@ -8,6 +9,7 @@ var styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#F5FCFF",
+        marginTop: 20
     },
     welcome: {
         fontSize: 20,
@@ -16,16 +18,48 @@ var styles = StyleSheet.create({
     },
     instructions: {
         textAlign: "center",
-        color: "#333333",
-        marginBottom: 5,
+        color: "#333333"
     },
 });
 
 class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+        var dates = [];
+        var currentDate = new Date().toJSON().slice(0,10);
+        this.state = {
+            currentDate: currentDate,
+        }
+    }
+
+    onDateSelect(date) {
+
+    }
+
     render(){
         return (
             <View style={styles.container}>
-                <Text>Calendar page</Text>
+                <CalendarComponent
+                customStyle={styles} // Customize any pre-defined styles
+                dayHeadings={['S', 'M', 'T', 'W', 'T', 'F', 'S']}               // Default: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+                //eventDates={}       // Optional array of moment() parseable dates that will show an event indicator
+                //events={[{date:'2015-07-01'}]}// Optional array of event objects with a date property and custom styles for the event indicator
+                monthNames={['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']}// Defaults to english names of months
+                nextButtonText={'Next'}           // Text for next button. Default: 'Next'
+                onDateSelect={(date) => this.onDateSelect(date)} // Callback after date selection
+                onSwipeNext={this.onSwipeNext}    // Callback for forward swipe event
+                onSwipePrev={this.onSwipePrev}    // Callback for back swipe event
+                onTouchNext={this.onTouchNext}    // Callback for next touch event
+                onTouchPrev={this.onTouchPrev}    // Callback for prev touch event
+                prevButtonText={'Prev'}           // Text for previous button. Default: 'Prev'
+                scrollEnabled={true}              // False disables swiping. Default: False
+                showControls={true}               // False hides prev/next buttons. Default: False
+                showEventIndicators={true}        // False hides event indicators. Default:False
+                startDate={this.state.currentDate}          // The first month that will display. Default: current month
+                titleFormat={'MMMM YYYY'}         // Format for displaying current month. Default: 'MMMM YYYY'
+                today={this.state.currentDate}              // Defaults to today
+                weekStart={1} // Day on which week starts 0 - Sunday, 1 - Monday, 2 - Tuesday, etc, Default: 1
+                />
             </View>
         );
     }
