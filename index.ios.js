ab72f9b3a58b36091fa24f17532d4dbaf2f070ed
Lucas Willemot <willemotlucas@gmail.com>
2017-01-06 10:26:42 +0100

    adding fonts to Xcode project to support iOS app

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,62 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import {Scene, Router, Actions, Modal} from 'react-native-router-flux';
+
+import Login from './components/login/Login';
+import Register from './components/signup/Register';
+import Search from './components/search/Search';
+import SearchCity from './components/search/SearchCity';
+import Profile from './components/profile/Profile';
+import Calendar from './components/calendar/Calendar';
+import Messages from './components/chat/Messages';
+import TabView from './components/common/TabView';
+import TabIcon from './components/common/TabIcon';
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    backgroundColor: 'transparent', 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabBarStyle: {
+    backgroundColor: '#F6F6F6',
+    borderTopColor: 'grey',
+  },
+  navigationBarStyle: {
+    backgroundColor: '#009286',
+    borderBottomColor: 'transparent',
+  },
+  titleStyle: {
+    color: 'white',
+  }
+});
+
+const scenes = Actions.create(
+  <Scene key="modal" component={Modal}>
+    <Scene key="root">
+      <Scene key="login" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} component={Login} initial title="Connexion"/>
+      <Scene key="register" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} component={Register} title="Inscription"/>
+      <Scene key="tabbar">
+        <Scene key="main" tabs tabBarStyle={styles.tabBarStyle}>
+          <Scene key="calendar" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Calendar} title="Calendrier"/>
+          <Scene key="search" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Search} title="Rechercher" initial={true}/>
+          <Scene key="profile" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Profile} title="Profil"/>
+          <Scene key="messages" navigationBarStyle={styles.navigationBarStyle} titleStyle={styles.titleStyle} icon={TabIcon} component={Messages} title="Messages"/>
+        </Scene>
+      </Scene>
+    </Scene>
+  </Scene>
+);
 
 export default class CouchUTC extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Router scenes={scenes}/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('CouchUTC', () => CouchUTC);
