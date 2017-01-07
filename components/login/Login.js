2879e1288fd54c9efaed0d07a26113300832e5b5
Lucas Willemot <willemotlucas@gmail.com>
2017-01-07 11:00:51 +0100

    Add Realm database + add User Schema and fake data when loading login page

--- components/login/Login.js
+++ components/login/Login.js
@@ -3,6 +3,9 @@ import {View, Text, StyleSheet} from "react-native";
 import {Actions} from "react-native-router-flux";
 import Button from 'react-native-button'
 
+import Realm from 'realm';
+import User from '../../models/User';
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -24,7 +27,67 @@ const styles = StyleSheet.create({
 
 
 export default class extends React.Component {
+    writeData() {
+        let realm = new Realm({schema: [User]});
+        let users = realm.objects('User');
+
+        if(users.length == 0) {
+            realm.write(() => {
+              realm.create('User', {
+                id: 1,
+                firstName:  'Lucas',
+                lastName: 'Willemot',
+                birthday: new Date('1994-04-30'),
+                gender: 'Male',
+                biography: 'Je vous attends !',
+                visitedCountries: 'France, Allemagne, Indonésie, Croatie, Portugal, Italie',
+                smoker: false,
+                hosting: true,
+                speciality: 'Génie Informatique',
+                phoneNumber: '0661065110',
+                createdAt: new Date(),
+                updatedAt: new Date()
+              });
+
+            realm.create('User', {
+                id: 2,
+                firstName:  'Valentin',
+                lastName: 'Paul',
+                birthday: new Date('1994-05-14'),
+                gender: 'Male',
+                biography: 'Je vous attends !',
+                visitedCountries: 'France, Allemagne, Indonésie, Croatie, Portugal, Italie',
+                smoker: false,
+                hosting: true,
+                speciality: 'Génie Informatique',
+                phoneNumber: '0661065110',
+                createdAt: new Date(),
+                updatedAt: new Date()
+              });
+
+            realm.create('User', {
+                id: 3,
+                firstName:  'Alexandra',
+                lastName: 'Duval',
+                birthday: new Date('1994-08-15'),
+                gender: 'Female',
+                biography: 'Je vous attends !',
+                visitedCountries: 'France, Allemagne, Indonésie, Croatie, Portugal, Italie',
+                smoker: false,
+                hosting: true,
+                speciality: 'Génie Informatique',
+                phoneNumber: '0661065110',
+                createdAt: new Date(),
+                updatedAt: new Date()
+              });
+            });
+        }
+    }
+
     render(){
+        console.log('render search');
+        this.writeData();
+
         const title = this.props.title || 'No Title';
         const data = this.props.data || 'No Data';
         return (
---
+++ models/User.js
@@ -0,0 +1,32 @@
+import Realm from 'realm';
+
+const UserSchema = {
+  name: 'User',
+  properties: {
+  	id: 'int',
+    firstName: 'string',
+    lastName: 'string',
+    birthday: 'date',
+    gender: 'string',
+    profilePicture: {type: 'data', optional: true},
+    biography: 'string',
+    visitedCountries: 'string',
+    smoker: 'bool',
+    hosting: 'bool',
+    speciality: 'string',
+    phoneNumber: 'string',
+    createdAt: 'date',
+    updatedAt: 'date'
+  }
+};
+
+class User {
+	age() {
+		return Math.floor(Math.floor((Date.now() - this.birthday.getTime())) / 31557600000);
+	}
+}
+
+User.schema = UserSchema;
+
+
+module.exports = User;
