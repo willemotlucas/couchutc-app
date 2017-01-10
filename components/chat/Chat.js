0ebe4a305911cf892bdf564899031833256f5eed
Alexandra DUVAL <duvalale@etu.utc.fr>
2017-01-10 22:35:51 +0100

    refresh when getting back to conversations list

--- components/chat/Chat.js
+++ components/chat/Chat.js
@@ -17,6 +17,7 @@ import User from '../../models/User';
 
 var styles = StyleSheet.create({
     container: {
+        flex: 1,
         justifyContent: "center",
         backgroundColor: "#F5FCFF",
     },
